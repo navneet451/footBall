290af38eb0104f6503e8e2953926460098889b2a
navneet451 <navneetthakur451@gmail.com>
2025-12-12 18:12:47 +0530

    second commit

--- controllers/organizer.controller.js
+++ controllers/organizer.controller.js
@@ -4,9 +4,10 @@ import Team from "../schemas/team.schema.js";
 export const createTeam = async (req, res, next) => {
   try {
     const {teamName, players} = req.body;
+     const playerId = players.map((pl)=> pl._id);
     const team = new Team({
         name: teamName,
-        players: players
+        players: playerId
     })
     await team.save();
     res.status(201).json({success: true, message: "team created", team});
@@ -22,5 +23,14 @@ export const getAllPlayers = async (req, res, next) => {
     res.status(200).json({message:"players fetched successfully", players});
   } catch (error) {
     res.status(500).json({message:"server erroe", success:false});
+  }
+}
+
+export const updateScore = async (req, res, next) => {
+  try {
+    const {team, oppositeTeam, teamScore, oppositeTeamScore} = req.body;
+
+  } catch (error) {
+    
   }
 }
