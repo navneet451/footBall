8f5f15159a5efc225af02e4a0269c2dbffca501f
navneet451 <navneetthakur451@gmail.com>
2025-12-13 15:58:49 +0530

    env changes

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 import express from "express";
-import { MONGOURI, PORT } from "./secret.js";
+import { CLIENTURI, MONGOURI, PORT } from "./secret.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import organizerRouter from "./routes/organizer.route.js";
 import mongoose from "mongoose";
 
-
 const app = express();
-
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: `${CLIENTURI}`,
+    credentials: true,
+  })
+);
 
-app.get("/test", (req,res)=>{
+app.get("/test", (req, res) => {
   res.send("hello world");
-})
+});
 
 app.use("/api/auth", userRouter);
 app.use("/api", organizerRouter);
-
 
 main()
   .then((res) => {
@@ -34,7 +36,6 @@ async function main() {
   await mongoose.connect(MONGOURI);
 }
 
-
 app.use((err, req, res, next) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || "Internal Server Error";
@@ -47,8 +48,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
--- secret.js
+++ secret.js
@@ -8,3 +8,4 @@ if (process.env.NODE_ENV != "production") {
 export const PORT = process.env.PORT || 3000;
 export const JWTSECRETS = process.env.JWT_SECRETS;
 export const MONGOURI=process.env.MONGO_URI;
+export const CLIENTURI=process.env.CLIENT_URI;
